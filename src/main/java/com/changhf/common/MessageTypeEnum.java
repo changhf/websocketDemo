a56a55660d69bf8c7ed9e369705256cfacbb7feb
常华锋 <changhuafeng@zafh.com.cn>
2018-10-26 14:54:14 +0800

    修改上下文

--- src/main/java/com/changhf/common/MessageTypeEnum.java
+++ src/main/java/com/changhf/common/MessageTypeEnum.java
@@ -1,32 +1,33 @@
 package com.changhf.common;
+
 /**
  * 消息类型enum
- * @author changhf
  *
+ * @author changhf
  */
 public enum MessageTypeEnum {
-	NOTICE(1, "通知"), TEXT_MSG(2, "文本消息");
-	private int code;
-	private String msg;
+    NOTICE(1, "通知"), TEXT_MSG(2, "文本消息");
+    private int code;
+    private String msg;
 
-	MessageTypeEnum(int code, String msg) {
-		this.code = code;
-		this.msg = msg;
-	}
+    MessageTypeEnum(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
 
-	public int getCode() {
-		return code;
-	}
+    public int getCode() {
+        return code;
+    }
 
-	public void setCode(int code) {
-		this.code = code;
-	}
+    public void setCode(int code) {
+        this.code = code;
+    }
 
-	public String getMsg() {
-		return msg;
-	}
+    public String getMsg() {
+        return msg;
+    }
 
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 }
--- src/main/java/com/changhf/service/ContentData.java
+++ src/main/java/com/changhf/service/ContentData.java
@@ -1,32 +1,32 @@
 package com.changhf.service;
 
 public class ContentData {
-	private String to;
-	private String msgData;
-	private int msgType;
+    private String to;
+    private String msgData;
+    private int msgType;
 
-	public String getTo() {
-		return to;
-	}
+    public String getTo() {
+        return to;
+    }
 
-	public void setTo(String to) {
-		this.to = to;
-	}
+    public void setTo(String to) {
+        this.to = to;
+    }
 
-	public String getMsgData() {
-		return msgData;
-	}
+    public String getMsgData() {
+        return msgData;
+    }
 
-	public void setMsgData(String msgData) {
-		this.msgData = msgData;
-	}
+    public void setMsgData(String msgData) {
+        this.msgData = msgData;
+    }
 
-	public int getMsgType() {
-		return msgType;
-	}
+    public int getMsgType() {
+        return msgType;
+    }
 
-	public void setMsgType(int msgType) {
-		this.msgType = msgType;
-	}
+    public void setMsgType(int msgType) {
+        this.msgType = msgType;
+    }
 
 }
